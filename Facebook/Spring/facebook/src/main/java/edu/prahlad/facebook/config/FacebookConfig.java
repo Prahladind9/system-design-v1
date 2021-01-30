087df8b4eb80a427346377d7892a2efa3fdacda4
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-30 19:48:15 +0530

    DB Connection Config

--- Facebook/Spring/facebook/src/main/java/edu/prahlad/facebook/config/FacebookConfig.java
+++ Facebook/Spring/facebook/src/main/java/edu/prahlad/facebook/config/FacebookConfig.java
@@ -1,15 +1,57 @@
 package edu.prahlad.facebook.config;
 
 import edu.prahlad.facebook.loader.YamlPropertyLoaderFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.sql.DataSource;
 
 @Configuration
 @PropertySource(
-        value = "file:{PROPERTY_PATH}",
+        value = "file:${PROPERTY_PATH}",
         factory = YamlPropertyLoaderFactory.class
 )
 @EnableAsync
 public class FacebookConfig {
+
+
+    @Value("${facebook.datasource.url}")
+    public String dataSourceUrl;
+
+    @Value("${facebook.datasource.username}")
+    public String dataSourceUsername;
+
+    @Value("${facebook.datasource.password}")
+    public String dataSourcePassword;
+
+    @Value("${facebook.datasource.driver}")
+    public String dataSourceDriver;
+
+    @Value("${facebook.threadPoolTaskExecutor.maxPoolSize}")
+    public Integer maxPoolSize;
+
+    @Value("${facebook.threadPoolTaskExecutor.corePoolSize}")
+    public Integer corePoolSize;
+
+    @Value("${facebook.threadPoolTaskExecutor.queueCapacity}")
+    public Integer queueCapacity;
+
+
+    @Value("${facebook.applicationName}")
+    public String applicationName;
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(dataSourceDriver);
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
+        return dataSource;
+    }
 }
