9e80f4a4ba5ef03939340d8ffa43b3a18487f763
Prahlad Rao <prahladskingdom@hotmail.com>
2021-07-16 23:03:20 +0530

    Netflix - Feature 2 - FetchTopMovies - 1

---
+++ Netflix/Java/NetflixFeatures/src/features/F_2_FetchTopMovies.java
@@ -0,0 +1,26 @@
+package features;
+
+/**
+ * Feature #2: Fetch Top Movies
+ */
+public class F_2_FetchTopMovies {
+
+    /**
+     * 0) Visualize the problem
+     * 1) Verify the constraints
+     * 2) Write test cases
+     *
+     *
+     * 3) Figure out a solution without code
+     *      a) Combining lists of multiple ratings into one
+     *
+     *
+     * 4) Write solution
+     * 5) Double check for errors - syntax/errors
+     * 6) Test code
+     * 7) Analysis
+     *      TimeComplexity:
+     *      SpaceComplexity:
+     */
+
+}
