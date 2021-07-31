cfbb09145d71058de6bb79e2287cfe5930999c78
Prahlad Rao <prahladskingdom@hotmail.com>
2021-07-31 23:19:57 +0530

    SearchEngine_F_2_AutocompleteSystem

---
+++ SearchEngine/Java/SearchEngineFeatures/src/features/v1/f2/F_2_AutocompleteSystem.java
@@ -0,0 +1,112 @@
+package features.v1.f2;
+
+import java.util.*;
+
+/**
+ * Feature #2: Design Search Autocomplete System
+ */
+public class F_2_AutocompleteSystem {
+    public static void main( String args[] ) {
+        String[] sentences = {"beautiful", "best quotes", "best friend", "best birthday wishes", "instagram", "internet"};
+        int[] times = {30, 14, 21, 10, 10, 15};
+        AutocompleteSystem auto = new AutocompleteSystem(sentences, times);
+        System.out.println(Arrays.toString(auto.autoComplete('b')));
+        System.out.println(Arrays.toString(auto.autoComplete('e')));
+        System.out.println(Arrays.toString(auto.autoComplete('s')));
+        System.out.println(Arrays.toString(auto.autoComplete('t')));
+        System.out.println(Arrays.toString(auto.autoComplete('#')));
+    }
+}
+
+class Node implements Comparable<Node>{
+    private static final int HOT_TOP = 3;
+    public HashMap<Character, Node> children;
+    public boolean isEnd;
+    public String data;
+    int rank;
+    public List<Node> hot;
+
+    public Node(){
+        this.children = new HashMap<>();
+        this.isEnd = false;
+        this.rank = 0;
+        hot = new ArrayList<>();
+    }
+
+    @Override
+    public int compareTo(Node n) {
+        if(this.rank == n.rank)
+            return this.data.compareTo(n.data);
+        return n.rank - this.rank;
+    }
+
+    public void update(Node n){
+        if(!this.hot.contains(n)){
+            this.hot.add(n);
+        }
+        Collections.sort(hot);
+        if(hot.size() > HOT_TOP){
+            hot.remove(hot.size() -1);
+        }
+    }
+}
+
+class AutocompleteSystem {
+    private Node root;
+    private Node current;
+    private String keyword;
+
+
+    public AutocompleteSystem(String[] sentences, int[] times){
+        this.root = new Node();
+        this.current = root;
+        this.keyword = "";
+
+        for (int i = 0; i < sentences.length; i++){
+            this.addRecord(sentences[i], times[i]);
+        }
+    }
+
+    public void addRecord(String sentence, int t){
+        Node node = this.root;
+        List<Node> visited = new ArrayList<>();
+        for (Character c:  sentence.toCharArray()){
+            if (!node.children.containsKey(c))
+                node.children.put(c, new Node());
+            node = node.children.get(c);
+            visited.add(node);
+        }
+        node.isEnd = true;
+        node.data = sentence;
+        node.rank += t;
+
+        for (Node i: visited){
+            i.update(node);
+        }
+    }
+
+    public String[] autoComplete(char c){
+        List<String> res = new ArrayList<>();
+        if (c == '#') {
+            addRecord(keyword, 1);
+            keyword = "";
+            current = root;
+            return new String[]{};
+        }
+
+        this.keyword += c;
+        if (current != null) {
+            if (!current.children.containsKey(c))
+                return new String[]{};
+            else
+                current = current.children.get(c);
+        }
+        else
+            return new String[]{};
+
+        for (Node node : current.hot) {
+            res.add(node.data);
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
